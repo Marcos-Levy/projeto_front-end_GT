3512dd21f358798b3794bc5804ab3ce3f4c2dc6f
Marcos Levy <104594877+Marcos-Levy@users.noreply.github.com>
2024-11-12 15:14:11 -0300

    Update productListingPage.jsx

--- src/pages/productListingPage.jsx
+++ src/pages/productListingPage.jsx
@@ -3,7 +3,9 @@ import { ProductsList } from "../components/sectionProductsListing";
 export const Produtos = () => {
     return ( 
         <>
+            <div clasname="bg-[#F9F8FE]">
             < ProductsList />
+            </div>
         </>
      );
 }
